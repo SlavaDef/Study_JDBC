083a4b249b4e3b291c85bd627f22db8f8df26bf2
SlavaDef <slavadef@gmail.com>
2024-03-16 16:10:31 +0200

    commit 4

--- src/main/java/org/example/Way5.java
+++ src/main/java/org/example/Way5.java
@@ -7,35 +7,33 @@ import static org.example.Constants.*;
 public class Way5 {
 
     public static void main(String[] args) throws SQLException {
-        // jdbc:h2:mem: тут закодована вся інфа для DriverManager для підключення до бд
-        // Connection  - отримання конекшену до бд
-        try (Connection con = DriverManager.getConnection(MSQL, "root", new Constants().getPAROLL());
-             Statement statement = con.createStatement()) {
-            createTables();
-            insertData(con);
-            readData(statement, SELECT_ALL);
-            readData(statement, SELECT_SECOND);
-        }
+
+        startApp();
 
     }
 
-    private static void createTables() throws SQLException {
+
+    private static void startApp() {
         try (Connection con = DriverManager.getConnection(MSQL, "root", new Constants().getPAROLL());
              Statement statement = con.createStatement()) {
-            // далі завдяки стетментам створюємо таблицю
             statement.execute("DROP TABLE IF EXISTS robot");
             statement.execute("CREATE TABLE robot(val INT, name VARCHAR(100))");
+            insertData(con, 10);
+            readData(statement, SELECT_ALL);
+            readData(statement, SELECT_SECOND);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private static void insertData(Connection connection) throws SQLException {
+    private static void insertData(Connection connection, int countOfEntity) throws SQLException {
         boolean autoComit = connection.getAutoCommit();
         connection.setAutoCommit(false); //знімаємо автопідтвердженя дій авто коміт
         // завдяки PreparedStatement легше і уникати дублювання
         try (PreparedStatement preparedStatement = connection.prepareStatement(
                 "INSERT  INTO  robot(val, name) VALUES (?, ?)")) {
 // кожний запит в бд це окрема транзакція ніби відкрили - зберігли - закомітили чи зробили роулбек якщо помилка
-            for (int i = 1; i < 12; i++) {
+            for (int i = 1; i <= countOfEntity; i++) {
                 preparedStatement.setInt(1, i);
                 preparedStatement.setString(2, "robot" + i);
                 preparedStatement.execute();
@@ -65,4 +63,14 @@ public class Way5 {
             throw new RuntimeException(e);
         }
     }
+
+
+     /*  private static void createTables() throws SQLException {
+        try (Connection con = DriverManager.getConnection(MSQL, "root", new Constants().getPAROLL());
+             Statement statement = con.createStatement()) {
+            // далі завдяки стетментам створюємо таблицю
+            statement.execute("DROP TABLE IF EXISTS robot");
+            statement.execute("CREATE TABLE robot(val INT, name VARCHAR(100))");
+        }
+    } */
 }
