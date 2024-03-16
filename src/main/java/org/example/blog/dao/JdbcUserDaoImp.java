0f66c1ccc50300486be2d23dde6bce83fdd1f0bf
SlavaDef <slavadef@gmail.com>
2024-03-16 18:42:38 +0200

    commit 7

--- src/main/java/org/example/blog/dao/JdbcUserDaoImp.java
+++ src/main/java/org/example/blog/dao/JdbcUserDaoImp.java
@@ -11,10 +11,18 @@ import static org.example.robot.Constants.*;
 
 public class JdbcUserDaoImp implements UserDao {
 
+    private final Connection connection;
+    private final Statement st;
+
+    public JdbcUserDaoImp() throws SQLException {
+        connection = DriverManager.getConnection(MYSQL,"root",new Constants().getPAROLL());
+        st = connection.createStatement();
+    }
+
 
     @Override
     public void save(User user) {
-        try (Connection connection = DriverManager.getConnection(MYSQL, "root", new Constants().getPAROLL());
+        try (//Connection connection = DriverManager.getConnection(MYSQL, "root", new Constants().getPAROLL());
              PreparedStatement pr = connection.prepareStatement(INSERT_INTO_USERS)) {
             pr.setString(1, user.getFullName());
             pr.setString(2, user.getPseudonym());
@@ -27,9 +35,9 @@ public class JdbcUserDaoImp implements UserDao {
 
     @Override
     public List<User> getAll() {
-        try (Connection connection = DriverManager.getConnection(MYSQL, "root", new Constants().getPAROLL());
-             Statement st = connection.createStatement();
-             ResultSet rs = st.executeQuery("SELECT * FROM users")) {
+        try {//(Connection connection = DriverManager.getConnection(MYSQL, "root", new Constants().getPAROLL());
+            // Statement st = connection.createStatement();
+             ResultSet rs = st.executeQuery("SELECT * FROM users");
             List<User> users = new ArrayList<>();
             while (rs.next()) {
                 String fullName = rs.getString("fullName");
@@ -37,6 +45,7 @@ public class JdbcUserDaoImp implements UserDao {
                 String email = rs.getString("email");
                 users.add(new User(fullName, pseudonym, email));
             }
+
             return users;
 
         } catch (SQLException e) {
@@ -46,8 +55,9 @@ public class JdbcUserDaoImp implements UserDao {
 
     @Override
     public void createTable() {
-        try (Connection connection = DriverManager.getConnection(MYSQL, "root", new Constants().getPAROLL());
-             Statement st = connection.createStatement()) {
+        try //(Connection connection = DriverManager.getConnection(MYSQL, "root", new Constants().getPAROLL());
+            // Statement st = connection.createStatement()) {
+        {
             st.execute("DROP TABLE IF EXISTS users");
               st.execute(CREATE_USERS);
         } catch (SQLException e) {
--- src/test/java/JdbcUserDaoImpTest.java
+++ src/test/java/JdbcUserDaoImpTest.java
@@ -2,6 +2,7 @@ import org.example.blog.dao.JdbcUserDaoImp;
 import org.example.blog.entyti.User;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,7 +12,7 @@ public class JdbcUserDaoImpTest {
 
 
     @Test
-    void getAll_shouldReturnSavedUsers() {
+    void getAll_shouldReturnSavedUsers() throws SQLException {
         JdbcUserDaoImp daoImp = new JdbcUserDaoImp();
         daoImp.createTable();
         List<User> users = List.of(new User("userFullName","userPsevdo", "userAmail"),
